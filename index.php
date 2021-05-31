e05771a6b8e642daebe3472c77d8e23d6cacf2ec
rafiffuaddo <rafiffuad3108@gmail.com>
2021-05-31 21:44:39 +0700

    ok

--- index.php
+++ index.php
@@ -60,6 +60,16 @@
                 </table> 
             </div> 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3..5.1.js"></script>
+            <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+            <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    $('#tabelMahasiswa').DataTable();
+                });
+            </script>
+        </body> 
+        </html>
             <!-- Button trigger modal -->
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahData">
                 Tambah Data
