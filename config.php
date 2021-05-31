df1699c43cc9f2902832955e1638df02c1c907f3
rafiffuaddo <rafiffuad3108@gmail.com>
2021-06-01 00:21:25 +0700

    ok

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root","","mahasiswa");
-if(mysqli_connect_error()){
+
+$koneksi = mysqli_connect("localhost", "root", "", "mahasiswa");
+
+if (mysqli_connect_error()) {
     echo "koneksi database gagal : " . mysqli_connect_error();
 }
